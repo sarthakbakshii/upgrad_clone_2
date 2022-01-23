13a4df02c5285152c2b3443f00a5e5200e1a4732
sarthakbakshii <sarthakbakshi14@gmail.com>
2022-01-23 12:21:57 +0530

    payment gateway

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Razorpay = require("razorpay")
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -28,5 +29,31 @@ app.use("/buy",purchasedCource)
 
 app.use("/courses",courceControler);
 
+// ========== payment gateway
+const razorpay = new Razorpay({
+    key_id : `rzp_test_Niuh6hOjeEJsU6`,
+    key_secret : `KHoRpRKWulpTZhZD29pbzMSd`
+})
+
+app.post("/order", (req, res) =>{
+
+    
+    razorpay.orders.create({
+        amount: Number(req.query.amount)*100,
+        currency: "INR",
+        receipt: "receipt#1",
+        notes: {
+            key1: "value3",
+            key2: "value2"
+        }
+        }, (err, order) => {
+            console.log(order);
+            res.json(order)
+        })
+})
+
 
 module.exports = app;
+
+// rzp_test_Niuh6hOjeEJsU6
+// KHoRpRKWulpTZhZD29pbzMSd
