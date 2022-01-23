5e4c9a39cdd407074892fce7a370110f65d18be2
sarthakbakshii <sarthakbakshi14@gmail.com>
2022-01-23 17:55:20 +0530

    a

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./index");
 const connect = require("./config/db")
+const port = process.env.PORT || 2345;
 
-app.listen( 2345 , async () => {
+app.listen( port , async () => {
     try {
         await connect();
         console.log("running on port 2345");
